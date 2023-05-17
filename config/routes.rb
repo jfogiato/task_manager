100cc43d513601b973b86a64f9b4b5b1b4ad9298
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-17 08:40:59 -0400

    Add: link to each task within task index view
    Add: base DB schema and initial migration

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   
   get "/tasks", to: "tasks#index"
   get "/tasks/new", to: "tasks#new"
+  get "/tasks/:id", to: "tasks#show"
   post "/tasks", to: "tasks#create"
 end
