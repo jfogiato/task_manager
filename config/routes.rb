f84dfce9b84a159d5c683f24e4225381992a5051
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-17 09:06:14 -0400

    Add: full CRUD functionality and completed app

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   get "/tasks/new", to: "tasks#new"
   get "/tasks/:id", to: "tasks#show"
   post "/tasks", to: "tasks#create"
+  get "/tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
